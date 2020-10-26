194d7f7eb3eda5686133d683d1d3fafb5d1c3f41
robertnetti <70272243+robertnetti@users.noreply.github.com>
2020-10-26 13:10:04 -0400

    cleaned up & commented code

--- Netti/multiFaceDetection/main.py
+++ Netti/multiFaceDetection/main.py
@@ -4,7 +4,6 @@ import face_recognition
 import os
 import glob
 import datetime
-import textwrap
 
 
 def greeting():
@@ -13,87 +12,98 @@ def greeting():
     if currentTime.hour < 12:
         return 'Good Morning, '
     elif 12 <= currentTime.hour < 18:
-        return 'Good afternoon, '
+        return 'Good Afternoon, '
     else:
-        return 'Good evening, '
+        return 'Good Evening, '
 
 
-faces_encodings = []
-faces_names = []
+def main():
+    faces_encodings = []
+    faces_names = []
+    face_locations = []
+    process_this_frame = True
 
-cur_direc = os.getcwd()
+    cur_direc = os.getcwd()
 
-path = os.path.join(cur_direc, 'data/faces/')
+    path = os.path.join(cur_direc, 'data/faces/')
 
-list_of_files = [f for f in glob.glob(path+'*.jpg')]
+    list_of_files = [f for f in glob.glob(path+'*.jpg')]
 
-number_files = len(list_of_files)
+    number_files = len(list_of_files)
 
-names = list_of_files.copy()
+    names = list_of_files.copy()
 
-for i in range(number_files):
-    globals()['image_{}'.format(i)] = face_recognition.load_image_file(list_of_files[i])
-    globals()['image_encoding_{}'.format(i)] = face_recognition.face_encodings(globals()['image_{}'.format(i)])[0]
-    faces_encodings.append(globals()['image_encoding_{}'.format(i)])
+    # train program to associate names with images
+    for i in range(number_files):
+        globals()['image_{}'.format(i)] = face_recognition.load_image_file(list_of_files[i])
+        globals()['image_encoding_{}'.format(i)] = face_recognition.face_encodings(globals()['image_{}'.format(i)])[0]
+        faces_encodings.append(globals()['image_encoding_{}'.format(i)])
 
-    # Create array of known names
-    names[i] = names[i].replace(cur_direc, "")
-    faces_names.append(names[i])
+        # Create array of known names
+        names[i] = names[i].replace(cur_direc, "")
+        faces_names.append(names[i])
 
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
+    # get webcam
+    video_capture = cv2.VideoCapture(0)
 
-video_capture = cv2.VideoCapture(0)
+    while True:
+        # get the current frame
+        ret, frame = video_capture.read()
 
-while True:
-    # grab a single frame of video
-    ret, frame = video_capture.read()
+        # Resize the frame of video to 1/4 size for faster facial recognition
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        # Convert image from BGR to RGB for facial_recognition
+        rgb_small_frame = small_frame[:, :, ::-1]
 
-    rgb_small_frame = small_frame[:, :, ::-1]
+        if process_this_frame:
+            # find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-    if process_this_frame:
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            face_names = []
+            for face_encoding in face_encodings:
+                # see if the face is a match for the known faces
+                matches = face_recognition.compare_faces(faces_encodings, face_encoding)
+                name = "Unknown"
 
-        face_names = []
-        for face_encoding in face_encodings:
-            matches = face_recognition.compare_faces(faces_encodings, face_encoding)
-            name = "Unknown"
+                face_distances = face_recognition.face_distance(faces_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = faces_names[best_match_index]
 
-            face_distances = face_recognition.face_distance(faces_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = faces_names[best_match_index]
+                face_names.append(name)
 
-            face_names.append(name)
+        process_this_frame = not process_this_frame
 
-    process_this_frame = not process_this_frame
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
 
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+            # Draw a rectangle around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (39, 85, 55), 2)
 
-        # Draw a rectangle around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (39, 85, 55), 2)
+            # Input text label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (39, 85, 55), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
 
-        # Input text label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (39, 85, 55), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
+            text = greeting() + os.path.splitext(os.path.basename(name))[0] + '!'
 
-        text = greeting() + os.path.splitext(os.path.basename(name))[0] + '!'
+            cv2.putText(frame, text, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-        cv2.putText(frame, text, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
 
-    # Display the resulting image
-    cv2.imshow('Video', frame)
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # free up memory
+    video_capture.release()
+    cv2.destroyAllWindows()
 
+
+if __name__ == "__main__":
+    main()
