6161e7b3fbe20e9d1edcc9bba4b497ac42ff6d1e
Cj <cchiass2@oswego.edu>
2020-11-11 21:17:29 -0500

    Flask WebServer added Along with server communication

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import csv
 from flask import Flask, render_template, request, redirect, send_file
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import ValidationError,AnyOf
+from werkzeug.utils import secure_filename
+from wtforms import StringField
+from flask import Flask, request, redirect, render_template
+from paramiko import SSHClient
+from scp import SCPClient
+import os
+
+
+
+MAX_BYTE = 8294400
+
+def sendFile(pin,username,host):
+    ssh = SSHClient()
+    ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+    ssh.connect(host, username=username)
+    with SCPClient(ssh.get_transport()) as scp:
+        scp.put(pin+".jpeg", '/home/'+username+'/csc380/'+pin+".jpeg")# second parameter is what the name of the sent file is
 
 def getPins():
-    with open('pin.csv', newline='') as f:
+    with open('/home/cj/PycharmProjects/ImageTransporter/KIOSK-People-Counter/pin.csv', newline='') as f:
         reader = csv.reader(f)
         data = list(reader)
 
     return data[0]
-
-
-
-
-
-
 
 
 app = Flask(__name__)
@@ -25,6 +34,11 @@ class PinForm(FlaskForm):
     pin = StringField('Pin:')
     name = StringField('Name:')
 
+
+def upload_file():
+   if request.method == 'POST':
+
+      return 'file uploaded successfully'
 @app.route('/error')
 def error():
    return render_template('error.html')
@@ -33,8 +47,13 @@ def error():
 def form():
     form = PinForm()
 
+
     if request.method == 'POST':
         if form.pin.data in getPins():
+            print(form.pin.data)
+            f = request.files['file']
+            f.save(secure_filename(f.filename))
+
             return '<h1>Hello {}. you have been added!'.format(form.pin.data)
         else:
             return redirect('/error')#<h1>Error {}. not valid'.format(form.pin.data)
@@ -52,7 +71,17 @@ def zipdownload():
         return render_template('error.html')
 
 
+@app.route('/fileupload')
+def fileupload():
+    return render_template('fileupload.html')
 
+
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return "File saved successfully"
 
 
 if __name__ == '__main__':
