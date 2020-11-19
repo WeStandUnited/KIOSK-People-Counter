72b55ca164b5df8a20620cecdb0fc0c6ce8a8337
Gregory Maldonado <gmaldona@oswego.edu>
2020-11-19 02:10:58 -0500

    new SCP Script

--- SCPInitSender.py
+++ SCPInitSender.py
@@ -2,13 +2,30 @@ from paramiko import SSHClient
 from scp import SCPClient
 import os
 
+'''
+SendFile uses SCP to send file over network to another host
 
-def sendFile(pin,username,host):
+varibles:
+local_file- (str) that is the file location of the file you want to send on your local machine
+
+username - (str) username of account on server. NOTE: must have RSA key between server and your local machine
+
+server_local - (str) location on server you want the file NOTE: include the file you are sending
+
+eg :  
+server_local = "/home/myPC/video/file.mp3"
+username = "user"
+server_local = "/home/user/video/file.mp3"
+
+'''
+def sendFile(local_file,username,host,server_local):
     ssh = SSHClient()
     ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
     ssh.connect(host, username=username)
     with SCPClient(ssh.get_transport()) as scp:
-        scp.put(pin+".jpeg", '/home/'+username+'/csc380/'+pin+".jpeg")# second parameter is what the name of the sent file is
+        scp.put(local_file,server_local)
 
 
-sendFile("test4.jpeg",'cchiass2','pi.cs.oswego.edu')
+
+
+
